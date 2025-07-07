2b31cf743eee419b4a475e21b1d25ad5ba71cfd3
Roberto Armando Galina Juárez <195681655+RobertoGalina1@users.noreply.github.com>
2025-07-07 12:55:30 -0600

    Agregar funcion mostrar_resumen() de cada uno de los estudiantes

--- funciones.py
+++ funciones.py
@@ -2,20 +2,38 @@ def registrar_estudiante():
     estudiante = {}
     calificaciones = {}
     nombre = input("Ingrese el nombre completo del estudiante:")
-    edad = input("Ingresar la edad del estudiante:")
+    edad = int(input("Ingresar la edad del estudiante:"))
     materias = input("Ingrese las materias de inscritas en el semestre (separado por comas):").split(',')
     materias_tuplas = tuple(materias)
     for materia in materias_tuplas:
-        print("Materia: ",materia)
+        print("Materia:",materia)
         calificacion = float(input('Ingrese la calificación:'))
-        calificaciones[materia.lower()] = calificacion
+        calificaciones[materia] = calificacion
     estudiante_hash = list(str(int(hash(nombre))))
     estudiante_id = "".join(estudiante_hash[1::2])
     estudiante[estudiante_id] = {'nombre':nombre,
                                  'edad': edad,
                                  'materias del semestre':materias_tuplas,
                                  'calificaciones':calificaciones}
-    print(estudiante)
     return estudiante    
 
-registrar_estudiante()
+def mostrar_resumen(diccionario):
+    if not diccionario:
+        print("¡No se han registrado estudiantes!")
+        return
+    print("ESTUDIANTES REGISTRADOS")
+    for id,info in diccionario.items():
+        print("ID del estudiante: ",id)
+        print("Nombre: ",info['nombre'])
+        print("Edad: ",info['edad'])
+        print('Materias y Calificaciones')
+        print("*****************************************")
+        for materia,cali in info['calificaciones'].items():
+            print(materia," --- ",cali)
+        print("Promedio: ",sum(info['calificaciones'].values())/len(info['calificaciones']))
+        print("*****************************************")
+
+
+
+
+
